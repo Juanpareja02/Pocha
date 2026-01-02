07f344e7ceee9d35280d31599f61941da2266110
Juan Álvaro Pareja <parejajuan17@gmail.com>
2026-01-02 00:51:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8478836797-57045",
+  "appId": "1:326555016190:web:c89e21e645f5aea176feaf",
+  "apiKey": "AIzaSyBjnEhY2J6mhR3JW2WQH8Hd4GIUM9UHyMk",
+  "authDomain": "studio-8478836797-57045.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "326555016190"
+};
